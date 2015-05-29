90e1dddb0ec8d18cef2fe5be0cb51b75ccd1b229
Adam <adam.kinson@gmail.com>
2015-05-29 18:42:29 -0400

    add RPS files from prior project

---
+++ rockpaperkinson/lib/rockpaperkinson.rb
@@ -0,0 +1,7 @@
+require "rockpaperkinson/version"
+require "rockpaperkinson/rock_paper_scissors"
+require "rockpaperkinson/player"
+
+module Rockpaperkinson
+  # Your code goes here...
+end
---
+++ rockpaperkinson/lib/rockpaperkinson/version.rb
@@ -0,0 +1,3 @@
+module Rockpaperkinson
+  VERSION = "0.1.0"
+end
